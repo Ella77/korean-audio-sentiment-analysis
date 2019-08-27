806ea885f34c93dae7d6425227364b1be47c44c4
Ella77 <ksjbang@gmail.com>
2019-08-28 01:06:27 +0900

    modularization

---
+++ preprocessing/audio_utils.py
@@ -0,0 +1,12 @@
+#utils for audio
+'''
+-load_wav
+-remove silence
+ 묵음제거 https://github.com/carpedm20/multi-speaker-tacotron-tensorflow/blob/master/audio/silence.py
+-duration to frame
+-frame to duration
+    https://github.com/carpedm20/multi-speaker-tacotron-tensorflow/blob/master/audio/get_duration.py
+
+'''
+
+
---
+++ preprocessing/preprocess.py
@@ -0,0 +1,10 @@
+# 영상 -> 오디오  |  json 에서 발화시작, 종료시간별로 오디오 클립 분할 + bad data제거 (bad sampled)
+#
+#
+# sample rate, encoding error 거르는 전처리예시 https://github.com/homink/speech.ko
+
+
+'''
+1. json frame별로 오디오 클립 분할
+2. bad data제거
+'''
