4025b2247e51eb99c1e6bcee0ce43262462779db
Ellie Kang <ksjbang@gmail.com>
2019-10-10 04:21:35 +0900

    Update and rename datafeeder.py to tf_record.py

---
+++ tf_record.py
@@ -0,0 +1,96 @@
+import tensorflow as tf
+import numpy as np
+import glob
+
+ # get item 가져올때마다, dataloader
+ #
+def get_mfcc(filepath,feature_size): #TODO need to trim and split
+    import librosa
+    y, sr = librosa.load(filepath, sr=16000)
+    mfcc = librosa.feature.mfcc(y=y,sr=16000, hop_length=128,n_fft=512,n_mfcc=257) 
+
+def get_label(filepath):
+ json_name = filapath+'.json'
+ #TODO label = 
+ return label
+
+
+def _bytes_features(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _int64_features(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def write_tfrecords(tfrecord_path, sounds, labels, fs=16000):
+    with tf.python_io.TFRecordWriter(tfrecord_path) as writer:
+        for sound, label in zip(sounds, labels):
+      
+
+            example = tf.train.Example(features=tf.train.Features(
+                feature={
+                    'signal_raw': _bytes_features(sound),
+                    'sr': _int64_features(fs),
+                    'label': _int64_features(label)
+                }))
+            writer.write(example.SerializeToString())
+
+
+def create_tfrecords(wav_path, tfrecord_pathes,
+                    , fs=16000,
+                     ):
+    # tfrecord_pathes = pathes for train, val tfrecords
+    train_sounds, train_labels = [], []
+    val_sounds, val_labels = [], []
+
+    for wav in glob.glob(dataset):       #walk through dir
+   
+        
+        feature = get_mfcc(wav) 
+        label =get_label(wav)
+        #random seed and split into train and val data
+        train_sounds.append(feature)
+        train_labels.append(label)
+        val_sounds.append(feature)
+        val_labels.append(label)
+
+
+        print(len(train_sounds), len(train_labels))
+        print(len(val_sounds), len(val_labels))
+
+        print('Writing tfrecords...')
+        train_tfrecord_path, val_tfrecord_path = tfrecord_pathes
+
+        write_tfrecords(train_tfrecord_path, train_sounds, train_labels, fs=fs)
+        write_tfrecords(val_tfrecord_path, val_sounds, val_labels, fs=fs)    
+ 
+# pytorch vesion class BaseDataset(Dataset):
+#     def __init__(self, wav_paths, script_paths, bos_id=1307, eos_id=1308):
+#         self.wav_paths = wav_paths
+#         self.labels
+#         self.bos_id, self.eos_id = bos_id, eos_id
+
+#     def __len__(self):
+#         return len(self.wav_paths)
+
+#     def count(self):
+#         return len(self.wav_paths)
+
+#     def getitem(self, idx):
+#         #trim process 가져오기 (datautils에서)
+#         feat = get_mfcc(self.wav_paths[idx])
+#         label = get_label(self.script_paths[idx])
+#         return feat, label
+
+       
+if __name__ == "__main__":
+    SR = 16000
+
+    data_path = './datasets/'
+
+    wav_path = data_path + '.wav'
+    tfrecord_pathes = ['{}wav{}_train.tfrecord'.format(data_path, SR // 1000),
+                       '{}wav{}_val.tfrecord'.format(data_path, SRR // 1000)]
+
+    create_tfrecords(wav_path, tfrecord_pathes, fs=SR)       
